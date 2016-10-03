1afb1f45991e66c2f8531cb70ed774325fc5ffa0
Germano Corrêa <germanobruscato@gmail.com>
2016-10-03 09:47:06 -0300

    Implementado visualização de erros do MySQL

--- core/controller/Executor.php
+++ core/controller/Executor.php
@@ -4,7 +4,16 @@ class Executor {
 
 	public static function doit($sql){
 		$con = Database::getCon();
-		return array($con->query($sql),$con->insert_id);
+		// TODO: return mysql error
+		// $return = array($con->query($sql),$con->insert_id);
+
+		if (!$return = $con->query($sql))
+		{
+			echo '<script>alert("olá");</script>';
+			printf("Errormessage: %s\n", $con->error);
+			die;
+		}
+		return array($return, $con->insert_id);
 	}
 }
 ?>
