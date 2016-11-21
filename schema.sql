b68ef90f29786e5fe1ed0499b8b03e8a6493fced
Germano Corrêa <germanobruscato@gmail.com>
2016-11-21 09:46:19 -0200

    Modificado schema do BD

--- schema.sql
+++ schema.sql
@@ -1,96 +1,306 @@
-/**
-* @author evilnapsis
-* @brief Modelo de base de datos
-* @date 2015-10-24
-**/
-create database library;
+
+CREATE DATABASE library;
 use library;
 
-create table user (
-	id int not null auto_increment primary key,
-	name varchar(50) not null,
-	lastname varchar(50) not null,
-	username varchar(50) not null,
-	email varchar(255) not null,
-	password varchar(60) not null,
-	is_active boolean not null default 1,
-	is_admin boolean not null default 0,
-	created_at datetime not null
-);
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 21-Nov-2016 às 09:43
+-- Versão do servidor: 5.7.16-0ubuntu0.16.04.1
+-- PHP Version: 7.0.8-0ubuntu0.16.04.3
 
-insert into user (name,lastname,username,email,password,is_active,is_admin,created_at) value ("Administrador","do Sistema","admin","your@email.com",sha1(md5("admin")),1,1,NOW());
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-create table client (
-	id int not null auto_increment primary key,
-	name varchar(50) not null,
-	lastname varchar(50) not null,
-	email varchar(255) not null,
-	address varchar(60) not null,
-	phone varchar(60) not null,
-	is_active boolean not null default 1,
-	created_at datetime not null
-);
 
-create table author (
-	id int not null auto_increment primary key,
-	name varchar(200) not null,
-	lastname varchar(1000) not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table editorial (
-	id int not null auto_increment primary key,
-	name varchar(200) not null);
+--
+-- Database: `library`
+--
 
-create table category (
-	id int not null auto_increment primary key,
-	name varchar(200) not null
-);
+-- --------------------------------------------------------
 
-create table status (
-	id int not null auto_increment primary key,
-	name varchar(200) not null
-);
+--
+-- Estrutura da tabela `author`
+--
 
-insert into status (name) values ("Disponible"),("Ocupado"),("Inactivo");
+CREATE TABLE `author` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `lastname` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table book (
-	id int not null auto_increment primary key,
-	isbn varchar(100),
-	title varchar(200) not null,
-	subtitle varchar(1000) not null,
-	description varchar(1000) not null,
-	file varchar(255),
-	image varchar(255),
-	year int,
-	n_pag int,
-	author_id int,
-	editorial_id int,
-	category_id int,
-	foreign key (author_id) references author(id),
-	foreign key (editorial_id) references editorial(id),
-	foreign key (category_id) references category(id)
-);
+-- --------------------------------------------------------
 
-create table item(
-	id int not null auto_increment primary key,
-	code varchar(100),
-	status_id int not null,
-	foreign key (status_id) references status(id),
-	book_id int not null,
-	foreign key (book_id) references book(id)
-);
+--
+-- Estrutura da tabela `book`
+--
 
-create table operation(
-	id int not null auto_increment primary key,
-	item_id int not null,
-	client_id int not null,
-	start_at date not null,
-	finish_at date not null,
-	returned_at date,
-	user_id int not null,
-	receptor_id int ,
-	foreign key (client_id) references client(id),
-	foreign key (user_id) references user(id),
-	foreign key (receptor_id) references user(id),
-	foreign key (item_id) references item(id)
-);
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
+  `isbn` varchar(100) DEFAULT NULL,
+  `title` varchar(200) NOT NULL,
+  `subtitle` varchar(1000) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `n_pag` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `editorial_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(60) NOT NULL,
+  `phone` varchar(60) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `editorial`
+--
+
+CREATE TABLE `editorial` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `item`
+--
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL,
+  `code` varchar(100) DEFAULT NULL,
+  `status_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `operation`
+--
+
+CREATE TABLE `operation` (
+  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `start_at` date NOT NULL,
+  `finish_at` date NOT NULL,
+  `returned_at` date DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `receptor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `author_id` (`author_id`),
+  ADD KEY `editorial_id` (`editorial_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `editorial`
+--
+ALTER TABLE `editorial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `status_id` (`status_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `operation`
+--
+ALTER TABLE `operation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `receptor_id` (`receptor_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `editorial`
+--
+ALTER TABLE `editorial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `operation`
+--
+ALTER TABLE `operation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`),
+  ADD CONSTRAINT `book_ibfk_2` FOREIGN KEY (`editorial_id`) REFERENCES `editorial` (`id`),
+  ADD CONSTRAINT `book_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+--
+-- Limitadores para a tabela `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`),
+  ADD CONSTRAINT `item_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`);
+
+--
+-- Limitadores para a tabela `operation`
+--
+ALTER TABLE `operation`
+  ADD CONSTRAINT `operation_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `operation_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `operation_ibfk_3` FOREIGN KEY (`receptor_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `operation_ibfk_4` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+INSERT INTO user (name,lastname,username,email,password,is_active,is_admin,created_at) value ("Administrador","do Sistema","admin","your@email.com",sha1(md5("admin")),1,1,NOW());
