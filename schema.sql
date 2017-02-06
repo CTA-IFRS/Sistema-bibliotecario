4657d6a84d47518b85956290562dacb61aec45c6
Lael Nervis <nervys@users.noreply.github.com>
2017-02-06 14:08:07 -0300

    Update schema.sql
    Ajustes no schema.sql

--- schema.sql
+++ schema.sql
@@ -1,25 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 21-Dez-2016 às 10:37
--- Versão do servidor: 5.7.16-0ubuntu0.16.04.1
--- PHP Version: 7.0.8-0ubuntu0.16.04.3
+-- Generation Time: Fev 06, 2017 as 04:47 PM
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `library`
 --
-CREATE DATABASE IF NOT EXISTS `library` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `library`;
 
 -- --------------------------------------------------------
@@ -28,11 +19,12 @@ USE `library`;
 -- Estrutura da tabela `author`
 --
 
-CREATE TABLE `author` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
-  `lastname` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `lastname` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 -- --------------------------------------------------------
 
@@ -40,8 +32,8 @@ CREATE TABLE `author` (
 -- Estrutura da tabela `book`
 --
 
-CREATE TABLE `book` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `isbn` varchar(100) DEFAULT NULL,
   `title` varchar(200) NOT NULL,
   `subtitle` varchar(1000) NOT NULL,
@@ -52,8 +44,12 @@ CREATE TABLE `book` (
   `n_pag` int(11) DEFAULT NULL,
   `author_id` int(11) DEFAULT NULL,
   `editorial_id` int(11) DEFAULT NULL,
-  `category_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `category_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  KEY `editorial_id` (`editorial_id`),
+  KEY `category_id` (`category_id`)
+);
 
 -- --------------------------------------------------------
 
@@ -61,10 +57,11 @@ CREATE TABLE `book` (
 -- Estrutura da tabela `category`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 -- --------------------------------------------------------
 
@@ -72,16 +69,17 @@ CREATE TABLE `category` (
 -- Estrutura da tabela `client`
 --
 
-CREATE TABLE `client` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
   `address` varchar(60) NOT NULL,
   `phone` varchar(60) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1',
-  `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 -- --------------------------------------------------------
 
@@ -89,10 +87,11 @@ CREATE TABLE `client` (
 -- Estrutura da tabela `editorial`
 --
 
-CREATE TABLE `editorial` (
-  `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `editorial` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 -- --------------------------------------------------------
 
@@ -100,13 +99,16 @@ CREATE TABLE `editorial` (
 -- Estrutura da tabela `item`
 --
 
-CREATE TABLE `item` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(100) DEFAULT NULL,
   `patrimonio` varchar(250) DEFAULT NULL,
   `status_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `status_id` (`status_id`),
+  KEY `book_id` (`book_id`)
+);
 
 -- --------------------------------------------------------
 
@@ -114,16 +116,21 @@ CREATE TABLE `item` (
 -- Estrutura da tabela `operation`
 --
 
-CREATE TABLE `operation` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `operation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
   `start_at` date NOT NULL,
   `finish_at` date NOT NULL,
   `returned_at` date DEFAULT NULL,
   `user_id` int(11) NOT NULL,
-  `receptor_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `receptor_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`),
+  KEY `user_id` (`user_id`),
+  KEY `receptor_id` (`receptor_id`),
+  KEY `item_id` (`item_id`)
+);
 
 -- --------------------------------------------------------
 
@@ -131,8 +138,8 @@ CREATE TABLE `operation` (
 -- Estrutura da tabela `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
@@ -141,114 +148,21 @@ CREATE TABLE `user` (
   `is_active` tinyint(1) NOT NULL DEFAULT '1',
   `is_admin` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
-  `lang` VARCHAR(3) NOT NULL DEFAULT 'pt'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `author_id` (`author_id`),
-  ADD KEY `editorial_id` (`editorial_id`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `editorial`
---
-ALTER TABLE `editorial`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `status_id` (`status_id`),
-  ADD KEY `book_id` (`book_id`);
-
---
--- Indexes for table `operation`
---
-ALTER TABLE `operation`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `client_id` (`client_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `receptor_id` (`receptor_id`),
-  ADD KEY `item_id` (`item_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `author`
---
-ALTER TABLE `author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `book`
---
-ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `client`
---
-ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `editorial`
---
-ALTER TABLE `editorial`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `operation`
---
-ALTER TABLE `operation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Extraindo dados da tabela `user`
+--
+INSERT INTO user (name,lastname,username,email,password,is_active,is_admin,created_at) value ("Administrador","do Sistema","admin","your@email.com",sha1(md5("admin")),1,1,NOW());
+
+
 --
 -- Constraints for dumped tables
 --
@@ -275,9 +189,3 @@ ALTER TABLE `operation`
   ADD CONSTRAINT `operation_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `operation_ibfk_3` FOREIGN KEY (`receptor_id`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `operation_ibfk_4` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-INSERT INTO user (name,lastname,username,email,password,is_active,is_admin,created_at) value ("Administrador","do Sistema","admin","your@email.com",sha1(md5("admin")),1,1,NOW());
